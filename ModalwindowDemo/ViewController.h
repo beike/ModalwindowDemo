73ea528e6c19967d44be7385b335603a766544d7
becky <becky@beckydeMacBook-Pro.local>
2015-08-18 22:58:31 +0800

    Initial Commit

---
+++ ModalwindowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModalwindowDemo
+//
+//  Created by becky on 15/8/18.
+//  Copyright (c) 2015年 becky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ModalwindowDemoTests/ModalwindowDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  ModalwindowDemoTests.m
+//  ModalwindowDemoTests
+//
+//  Created by becky on 15/8/18.
+//  Copyright (c) 2015年 becky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ModalwindowDemoTests : XCTestCase
+
+@end
+
+@implementation ModalwindowDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
